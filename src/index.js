af52d77311f50a7e4775d180ef53e18293371a37
lttb <connect@lttb.net>
2017-04-28 16:08:09 +0300

    Check attributes by dom-elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import DOMProperty from 'react-dom/lib/DOMProperty'
-import EventPluginRegistry from 'react-dom/lib/EventPluginRegistry'
-
-
 const reactProps = {
   children: true,
   dangerouslySetInnerHTML: true,
@@ -19,26 +15,88 @@ const reactProps = {
   // deprecated
   valueLink: true,
   checkedLink: true,
+
+  // react different naming
+  allowFullScreen: true,
+
+  // include some
+  role: true,
+  scoped: true,
+
+  // events
+  onDoubleClick: true, // ondblclick
+
+  onAnimationStart: true,
+  onAnimationEnd: true,
+  onAnimationIteration: true,
+  onTransitionEnd: true,
+
+  // schema.org
+  itemProp: true,
+  itemScope:true,
+  itemType: true,
+  itemID: true,
+  itemRef: true,
 }
 
-const hasProp = Object.prototype.hasOwnProperty
+const NS = [
+  'http://www.w3.org/1999/xhtml',
+  'http://www.w3.org/2000/svg',
+]
 
-export default (prop: string) => {
-  if (
-    hasProp.call(DOMProperty.properties, prop)
-    || DOMProperty.isCustomAttribute(prop.toLowerCase())
-  ) {
+const elements = {}
+
+let doc = document
+
+export const init = (d = document) => {
+  doc = d
+}
+
+export const createDomElem = (name: string) => {
+  if (name in elements) return elements[name]
+
+  let domElem
+  for (let i = 0; i < NS.length; i += 1) {
+    domElem = doc.createElementNS(NS[i], name)
+    if (!(domElem instanceof HTMLUnknownElement)) {
+      elements[name] = domElem
+
+      return domElem
+    }
+  }
+
+  return domElem
+}
+
+const checkAttr = (domElem, attr) => {
+  if (reactProps[attr]) {
     return true
   }
-  if (hasProp.call(reactProps, prop) && reactProps[prop]) {
-    return true
-  }
-  if (
-    hasProp.call(EventPluginRegistry.registrationNameModules, prop)
-    || hasProp.call(EventPluginRegistry.registrationNameModules, prop.split('Capture')[0])
-  ) {
-    return true
-  }
+
+  if (attr in domElem) return true
+
+  const a = attr.toLowerCase()
+
+  if (a in domElem) return true
+  if (a.startsWith('data-')) return true
+  if (a.startsWith('aria-')) return true
+
+  const e = attr.split('Capture')[0]
+  if (e.toLowerCase() in domElem || e in reactProps) return true
 
   return false
+}
+
+export const validAttr = (attr: string) => {
+  const res = Object.keys(elements).some((name: string) => checkAttr(elements[name], attr))
+
+  return res
+}
+
+export default (elem: string) => (prop: string) => {
+  if (reactProps[prop]) {
+    return true
+  }
+
+  return checkAttr(createDomElem(elem), prop)
 }
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const env = process.env.NODE_ENV
+const isProd = env === 'production'
+const isDev = env === 'development'
+const isTest = env === 'test'
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(env),
+    __DEV__: isDev,
+    __TEST__: isTest
+  })
+]
+
+if (isProd) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }))
+}
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    library: 'jss',
+    libraryTarget: 'umd'
+  },
+  plugins,
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      // For plugins tests.
+      jss: path.join(__dirname, 'src')
+    }
+  }
+}
